3c44ff2e92f6a9c4bbb1d85711b5467f81a8502f
Derek Damron <damrondm@gmail.com>
2018-04-01 22:27:39 -0700

    Initialize repo as module with example Jupyter notebooks

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tictactoe',
+      version='0.1',
+      description='Implementation of tic-tac-toe with bots',
+      url='http://github.com/ddamv/tictactoe',
+      author='Derek Damron',
+      author_email='damrondm@gmail.com',
+      license='MIT',
+      packages=['tictactoe'],
+      zip_safe=False)
