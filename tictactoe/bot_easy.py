2b3c73e0c440feb284060e66394eb45851528f3a
Derek Damron <damrondm@gmail.com>
2018-04-13 08:23:38 -0700

    Rename bot_random to bot_easy

--- tictactoe/bot_easy.py
+++ tictactoe/bot_easy.py
@@ -1,6 +1,6 @@
 from random import sample
 
-def bot_random(game):
+def bot_easy(game):
     possible_moves = game.available_moves()
     random_move = [ possible_moves[i] for i in sorted(sample(range(len(possible_moves)), 1)) ][0]
     game.move(random_move)
