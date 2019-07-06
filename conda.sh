8dff06e8eb0cab9fb5620188cedc07bda39b4c4c
Derek Damron <damrondm@gmail.com>
2019-07-06 16:51:12 -0700

    Update conda scripts

--- conda.sh
+++ conda.sh
@@ -4,7 +4,25 @@
 #
 # Must be executed from inside the high-level tictactoe folder
 #
+# Possible args:
+# build: Build from scratch
+# update: Update current build
+#
 #####
 
-conda env create --name tictactoe --file conda.yml
+if [ "$1" == "build" ]; then
+    # Build from scratch
+    echo "Building from scratch"
+    conda env create --name tictactoe --file conda.yml
+elif [ "$1" == "update" ]; then
+    # Update
+    echo "Updating existing build"
+    conda env update --name tictactoe --file conda.yml
+else
+    # Error
+    echo "ERROR: Unrecognized argument '$1', use 'build' or 'update'" 1>&2
+    exit 1 # terminate and indicate error
+fi
+
+# Install local tictactoe
 pip install ./
