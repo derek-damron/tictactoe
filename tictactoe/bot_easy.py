e9b65e98e4bd67714a6ab185d3531d6044d42b98
Derek Damron <damrondm@gmail.com>
2019-07-06 10:27:28 -0700

    Use random choice instead of random sample for cleaner implementation

--- tictactoe/bot_easy.py
+++ tictactoe/bot_easy.py
@@ -1,6 +1,6 @@
-from random import sample
+from random import choice
 
 def bot_easy(game):
     possible_moves = game.available_moves()
-    random_move = [ possible_moves[i] for i in sorted(sample(range(len(possible_moves)), 1)) ][0]
+    random_move = choice(possible_moves)
     game.move(random_move)
